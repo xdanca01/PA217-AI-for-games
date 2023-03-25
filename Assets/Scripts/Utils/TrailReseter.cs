87500cc9f1d58e428788287810d09b318abe6c6c
xdanca01 <petrik2008@email.cz>
2023-03-25 15:14:07 +0100

    Adding Completed HW

---
+++ Assets/Scripts/Utils/TrailReseter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailReseter : MonoBehaviour
+{
+    [SerializeField]
+    private TrailRenderer trailRenderer;
+
+    private void OnDisable()
+    {
+        trailRenderer.Clear();
+    }
+}
