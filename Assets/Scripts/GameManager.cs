4d76f1264452a1adab9fac6e534a78e45d5249ea
xdanca01 <petrik2008@email.cz>
2023-04-07 18:39:55 +0200

    HW2

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,162 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+
+
+public class GameManager : MonoBehaviour
+{
+    public enum heuristics
+    {
+        Euclidean,
+        NULL
+    }
+    /// <summary>
+    /// Reference to the instance of GameManager class.
+    /// </summary>
+    /// 
+    public static GameManager Instance { get; private set; }
+
+    [Header("Object references")]
+    [SerializeField]
+    private Maze maze;
+
+    [SerializeField]
+    private GameObject agentPrefab;
+
+    [SerializeField]
+    private GameObject flagPrefab;
+
+    [Header("Other settings")]
+    [SerializeField]
+    private float movementSpeed = 5.0f;
+
+    [SerializeField] public float tickTime = 0.01f;
+
+    public heuristics Heuristic = heuristics.Euclidean;
+
+    public System.Action<Vector2Int> DestinationChanged;
+
+    /// <summary>
+    /// Reference to the current Maze instance.
+    /// </summary>
+    public Maze Maze => maze;
+
+    /// <summary>
+    /// Reference to the game object representing the destination flag.
+    /// </summary>
+    public GameObject DestinationFlag { get; private set; }
+
+    private Vector2Int _destinationTile;
+    /// <summary>
+    /// Corresponds to the tile index of the destination flag.
+    /// </summary>
+    public Vector2Int DestinationTile
+    {
+        get => _destinationTile;
+        set
+        {
+            _destinationTile = value;
+            DestinationChanged?.Invoke(_destinationTile);
+        }
+    }
+
+    private Camera _mainCamera;
+    public Camera MainCamera
+    {
+        get
+        {
+            if(_mainCamera == null)
+            {
+                _mainCamera = Camera.main;
+            }
+
+            return _mainCamera;
+        }
+    }
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Debug.LogError("Multiple instances of game manager occured.");
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        InitializeObjects();
+    }
+
+    private void Update()
+    {
+        CheckForNewDestination();
+    }
+
+    private void OnDestroy()
+    {
+        Instance = null;
+    }
+
+    public void InitializeObjects()
+    {
+        maze.BuildMaze();
+
+        SpawnAgent(agentPrefab, maze.AgentSpawnTilePos);
+        _destinationTile = maze.AgentSpawnTilePos;
+        DestinationFlag = SpawnFlag(flagPrefab);
+    }
+
+    private Agent SpawnAgent(GameObject agentPrefab, Vector2Int tilePos)
+    {
+        var agentGo = Instantiate(agentPrefab, Vector3.zero, Quaternion.identity);
+
+        var agentComp = agentGo.GetComponentInChildren<Agent>();
+
+        if (agentComp == null)
+        {
+            Debug.LogError("Invalid agent prefab: " + agentPrefab.name);
+        }
+        else
+        {
+            agentComp.InitializeData(maze, movementSpeed, tilePos);
+        }
+
+        return agentComp;
+    }
+
+    private GameObject SpawnFlag(GameObject flagPrefab)
+    {
+        var flagGo = Instantiate(flagPrefab, Vector3.zero, Quaternion.identity);
+
+        flagGo.transform.localScale = maze.GetElementsScale();
+        flagGo.SetActive(false);
+
+        return flagGo;
+    }
+
+    private void CheckForNewDestination()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Input.mousePosition;
+            mousePos.z = MainCamera.nearClipPlane;
+            var mouseWorldPos = MainCamera.ScreenToWorldPoint(mousePos);
+
+            var destTile = maze.GetMazeTileForWorldPosition(mouseWorldPos);
+
+            if(maze.IsValidTileOfType(destTile, MazeTileType.Free))
+            {
+                DestinationFlag.transform.position = maze.GetWorldPositionForMazeTile(destTile);
+                DestinationFlag.SetActive(true);
+                DestinationTile = destTile;
+            }
+        }
+    }
+}
